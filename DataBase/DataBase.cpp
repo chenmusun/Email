3fb2257186b5966a9dd8223e01d8d98bce27083a
zhangyu <yingxong-4402@126.com>
2015-03-12 14:14:08 +0800

    Add Databaselib
    DataBase DLL

---
+++ DataBase/DataBase.cpp
@@ -0,0 +1,178 @@
+// DataBase.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "DataBase.h"
+
+static long lCount = 1;
+// 这是导出变量的一个示例
+DATABASE_API int nDataBase = 0;
+
+// 这是导出函数的一个示例。
+DATABASE_API int fnDataBase(void)
+{
+	return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 DataBase.h
+CDataBase::CDataBase(void)
+{
+	memset(&m_dbinfo, 0, sizeof(MongoDBInfo));
+	return;
+}
+
+CDataBase::~CDataBase()
+{
+
+}
+
+BOOL CDataBase::ConnectDataBase(string& strErr)
+{
+	string strWhat;
+	strErr.empty();//将错误信息清空
+	if (m_dbinfo.nUseDB != 1)
+		return TRUE;
+	if (m_dbinfo.chDBAdd && m_dbinfo.chDBAdd[0] != '\0')
+	{
+		try
+		{
+			mongo::client::initialize();
+			if (connect.connect(m_dbinfo.chDBAdd, strErr))
+			{
+				connect.setSoTimeout(1000);
+				if (!connect.auth(m_dbinfo.chDBName, m_dbinfo.chUserName, m_dbinfo.chPasswd, strErr))//登陆数据库验证
+				{
+#ifdef _DEBUG
+					OutputDebugStringA(strErr.c_str());
+					OutputDebugStringA("\r\n");
+#endif
+					return FALSE;
+				}
+			}
+			else
+			{
+#ifdef _DEBUG
+				OutputDebugStringA(strErr.c_str());
+				OutputDebugStringA("\r\n");
+#endif
+				return FALSE;
+			}
+		}
+		catch (const mongo::DBException &e)
+		{
+
+			strWhat = e.what();
+			strWhat.append("\r\n");
+			strErr.append(strWhat);
+#ifdef _DEBUG
+			OutputDebugStringA(strErr.c_str());
+#endif
+			return FALSE;
+		}
+	}
+	return TRUE;
+}
+
+long CDataBase::ExecSQL(const string& strUIDL, string& strErr,const string& strName)
+{
+	//COleDateTime date;
+	strErr.empty();
+	long lFound = MONGO_FOUND;
+	if (m_dbinfo.nUseDB != 1)
+	{
+		lFound = MONGO_NOT_FOUND;
+		return lFound;
+	}
+	string strIndexName(m_dbinfo.chDBName);
+	strIndexName.append(".");
+	strIndexName.append(m_dbinfo.chTable);
+	if (connect.isStillConnected())
+	{
+		BSONObj cmd, obj = BSON("UIDL" << strUIDL << "MD5" << "MD5test" << "DATE" << DATENOW<<"TO"<<strName<<"STATUS"<<3)
+			, bsoReturnValue, bsoQuery = BSON("UIDL" << strUIDL), bsoValue;
+		try
+		{
+			bsoReturnValue = connect.findOne(strIndexName, bsoQuery, &bsoValue);
+			if (bsoReturnValue.isEmpty())
+			{
+#ifdef _DEBUG
+				char chDebugInfo[256] = { 0 };
+				sprintf_s(chDebugInfo, 256, "InsertCount = %d\r\n", lCount++);
+				OutputDebugStringA(chDebugInfo);
+#endif
+				connect.insert(strIndexName, obj);
+				lFound = MONGO_NOT_FOUND;
+			}
+			else
+			{
+				string strName("DATE"), strValue;
+				strValue = bsoReturnValue.toString();
+				mongo::BSONElement obj = bsoReturnValue.getField("DATE");
+				mongo::Date_t date = obj.Date();
+				long long lSrvDate = date.asInt64(), lLocalDate(0), lDay(1209600);
+				lSrvDate = lSrvDate / 1000;
+				lLocalDate = GetTimeStamp();
+				if (lLocalDate - lSrvDate > lDay)
+					lFound = MONGO_DELETE;
+			}
+
+		}
+		catch (const mongo::DBException &e)
+		{
+			strErr = e.what();
+		}
+	}
+	return lFound;
+}
+
+
+void CDataBase::DisConnectDataBase()
+{
+	BSONObj info;
+	if (m_dbinfo.nUseDB != 1) return;
+	if (connect.isStillConnected())
+	{
+		connect.logout(m_dbinfo.chDBName, info);
+	}
+}
+
+void CDataBase::SetDBInfo(const MongoDBInfo&dbinfo)
+{
+	memset(&m_dbinfo, 0, sizeof(MongoDBInfo));
+	memcpy_s(&m_dbinfo, sizeof(MongoDBInfo), &dbinfo, sizeof(MongoDBInfo));
+}
+
+void CDataBase::GetCurrTime(string& strDate)
+{
+	char chDate[128] = { 0 };
+	SYSTEMTIME systm;
+	GetLocalTime(&systm);
+	sprintf_s(chDate, 128, "%4d-%.2d-%.2d %.2d:%.2d:%.2d",
+		systm.wYear, systm.wMonth, systm.wDay,
+		systm.wHour, systm.wMinute, systm.wSecond);
+	strDate = chDate;
+}
+
+long long CDataBase::GetTimeStamp()
+{
+	long long llTime = 0;
+	time_t rawtime;
+	time(&rawtime);
+	llTime = (long long)rawtime;
+	return llTime;
+}
+
+void CDataBase::SaveFileToMongoDB(string& strRtr)
+{
+	/*
+	GfidFS 对象用来存储文件，构造是需要传入DBClientConnection实例，使用数据库名称
+	storeFile函数用来上传指定路径的文件，参数一：文件路径，参数二：数据库存储名称
+	write函数下载文件，参数：文件保存路径
+	*/
+	string remotename("test11.pdf"),strerr;
+	GridFS fs(connect,m_dbinfo.chDBName);
+	BSONObj obj=fs.storeFile("E:\\Mail\\MailTest\\ReceiveEmail\\ReceiveEmail\\Email\\912340\\2.pdf", remotename);
+	BSONElement em = obj.getField("md5");
+	strRtr = em.toString();
+}
---
+++ DataBase/DataBase.h
@@ -0,0 +1,47 @@
+// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 DATABASE_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何其他项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// DATABASE_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef DATABASE_EXPORTS
+#define DATABASE_API __declspec(dllexport)
+#else
+#define DATABASE_API __declspec(dllimport)
+#pragma	comment(lib,"../output/Database.lib")
+#endif
+#include <string>
+using namespace std;
+#include <winsock2.h>
+#include "mongo/client/dbclient.h"
+#include "mongo/bson/bson.h"
+using namespace mongo;
+#include "../ReceiveEmail/public.h"
+
+//#include <windows.h>
+// 此类是从 DataBase.dll 导出的
+class DATABASE_API CDataBase {
+public:
+	CDataBase(void);
+	~CDataBase();
+	// TODO:  在此添加您的方法。
+public:
+	BOOL ConnectDataBase(string& strErr);//连接数据库（格式：数据库IP地址:端口），连接之前需要设定表明、用户名、密码
+	long ExecSQL(const string& strUIDL, string& strErr,const string& strName);//检查UIDL是否存在，若不存在插入一条新数据
+	void DisConnectDataBase();//断开数据库连接（目前使用MongoAPI中的Logout）
+	void SetDBInfo(const MongoDBInfo&dbinfo);//设置数据库相关信息
+	inline string GetDBName(){ string strName(m_dbinfo.chDBName); return strName; }
+	void GetCurrTime(string& strDate);
+	long long GetTimeStamp();
+	inline int GetUseDB(){ return m_dbinfo.nUseDB; }
+	void SaveFileToMongoDB(string& strRtr);
+private:
+	DBClientConnection connect;//Mongo连接实例
+	MongoDBInfo m_dbinfo;
+private:
+protected:
+};
+
+extern DATABASE_API int nDataBase;
+
+DATABASE_API int fnDataBase(void);
